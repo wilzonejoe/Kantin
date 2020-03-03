684bfa1b01ddec22acf0bc26adb6db60398d92b8
William Anderson <anderson.william56968@gmail.com>
2020-03-03 15:02:48 +1300

    Adds Menu to Project. Adds Menu to KantinEntities. Renamed MenutItem to MenuItem.

--- Kantin.Data/Models/MenuAddOnItem.cs
+++ Kantin.Data/Models/MenuAddOnItem.cs
@@ -6,7 +6,7 @@ namespace Kantin.Data.Models
     {
         public int MenuItemId { get; set; }
         public int AddOnItemId { get; set; }
-        public MenuItem MenutItem { get; set; }
+        public MenuItem MenuItem { get; set; }
         public AddOnItem AddOnItem { get; set; }
 
         public MenuAddOnItem() : base() { }
